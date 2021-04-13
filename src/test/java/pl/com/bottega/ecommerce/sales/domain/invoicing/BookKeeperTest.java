828602c715fceb5ec0bb646ae16ed6213c17b91c
Mociej <maciekkocemba@gmail.com>
2021-04-13 15:27:12 +0200

    test case 1

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,10 +1,14 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
@@ -14,49 +18,87 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import java.util.Date;
 
+@ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
 
-    private static final String SAMPLE_CLIENT_NAME="Nowak";
+    private static final String SAMPLE_CLIENT_NAME = "Nowak";
+    private static final String SAMPLE_TAX_NAME = "tax1";
+    private static final String SAMPLE_PRODUCTDATA_NAME = "test1";
+    private static final Id ID_ONE = new Id("1");
+    private static final ClientData CLIENT_DATA = new ClientData(ID_ONE, SAMPLE_CLIENT_NAME);
+    private static final Tax TAX_ZERO = new Tax(new Money(0), SAMPLE_TAX_NAME);
+    private static final Money MONEY_ZERO = new Money(0);
+    private static final int QUANTITY_ONE = 1;
+    private static final int RESULT_COUNT_ONE = 1;
+
     @Mock
     private InvoiceFactory invoiceFactory;
+    @Mock
     private TaxPolicy taxPolicy;
+
     private BookKeeper bookKeeper;
-    private InvoiceRequest invoiceRequest;
-    private Invoice invoice;
 
-    private RequestItem requestItem;
-    private Money money;
-    private Tax tax;
-    private InvoiceLine invoiceLine;
-
-    private Id id;
-    private ClientData clientData;
-    private ProductData productData;
     private ProductDataBuilder productDataBuilder;
-    int quantity=1;
-
-
-
 
     @BeforeEach
     void setUp() throws Exception {
-        bookKeeper=new BookKeeper(invoiceFactory);
-
-        id=new Id("1");
-        clientData=new ClientData(id,SAMPLE_CLIENT_NAME);
-        invoice=new Invoice(id,clientData);
-        money=new Money(0);
-        productDataBuilder=new ProductDataBuilder();
-        productData=productDataBuilder.init(id,money,"test1",ProductType.STANDARD,new Date());
-
-        requestItem=new RequestItem(productData,quantity,money);
-        tax=new Tax(money,"tax1");
-        invoiceLine=new InvoiceLine(productData,quantity,money,tax);
-
+        bookKeeper = new BookKeeper(invoiceFactory);
+        productDataBuilder = new ProductDataBuilder();
     }
 
     @Test
-    void InvoiceOneWithOne() {
+    void InvokeInvoiceWithOneItemReturnInvoiceWithOneItem() {
+        //Given
+        InvoiceRequest invoiceRequest = new InvoiceRequest(CLIENT_DATA);
+
+        ProductData productData = productDataBuilder.init(ID_ONE, MONEY_ZERO, SAMPLE_PRODUCTDATA_NAME, ProductType.STANDARD, new Date());
+
+        RequestItem requestItem = new RequestItem(productData, QUANTITY_ONE, MONEY_ZERO);
+        invoiceRequest.add(requestItem);
+
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(TAX_ZERO);
+        when(invoiceFactory.create(CLIENT_DATA)).thenReturn(new Invoice(ID_ONE, CLIENT_DATA));
+
+        //When
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        //Then
+        assertNotEquals(null, invoice);
+        assertEquals(RESULT_COUNT_ONE, invoice.getItems().size());
     }
 
+    @Test
+    void TestCase2() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void TestState1() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void TestState2() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void TestBehaviour1() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void TestBehaviour2() {
+        //Given
+        //When
+        //Then
+    }
 }
